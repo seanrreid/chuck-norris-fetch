bf9339216695cbaa3ab7b29fe4a24f57ebf61f07
Sean R Reid <sean@torchcodelab.com>
2019-09-23 08:58:04 -0400

    Refactored to a starter state

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,10 @@
-"use strict"
+"use strict";
 
-const getMoreQuotesButton = document.getElementById('getMoreQuotes');
-getMoreQuotesButton.addEventListener('click', function(e){
-    e.preventDefault();
-    updateChuckSays();
-});
+const getMoreQuotesButton = document.getElementById("getMoreQuotes");
 
-function updateChuckSays() {
-    const chuckSays = document.getElementById('chuckSays');
-    
-    get('https://api.chucknorris.io/jokes/random?category=dev')
-    .then((response) => {
-        chuckSays.innerHTML = response;
-    });
-}
+// Add an event listener to the button, DON'T FORGET TO PREVENT THE DEFAULT BEHAVIOR!
+// Call a function to return a new quote, and update the DOM
+getMoreQuotesButton.addEventListener("click", function(e) {});
+
+// Create a function to update the quote text in the DOM
+function updateChuckSays() {}
