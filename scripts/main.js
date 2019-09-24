255b43e8a7c60beda533d380a1298e9d5b18e24f
Sean R Reid <sean@torchcodelab.com>
2019-09-24 07:49:54 -0400

    Moved initial function call back into main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,18 @@
-"use strict"
+"use strict";
 
-const getMoreQuotesButton = document.getElementById('getMoreQuotes');
-getMoreQuotesButton.addEventListener('click', function(e){
+const getMoreQuotesButton = document.getElementById("getMoreQuotes");
+getMoreQuotesButton.addEventListener("click", function(e) {
     e.preventDefault();
     updateChuckSays();
 });
 
 function updateChuckSays() {
-    const chuckSays = document.getElementById('chuckSays');
-    
-    get('https://api.chucknorris.io/jokes/random?category=dev')
-    .then((response) => {
-        chuckSays.innerHTML = response;
-    });
+    const chuckSays = document.getElementById("chuckSays");
+
+    get("https://api.chucknorris.io/jokes/random?category=dev").then(
+        response => {
+            chuckSays.innerHTML = response;
+        }
+    );
 }
+updateChuckSays();
