1958965ef5ef91aa11092f315b2eab8c48d128a6
Sean R Reid <sean@torchcodelab.com>
2019-09-24 08:27:38 -0400

    Added ability to dynamically add categories, and select quote based on those categories

--- scripts/get.js
+++ scripts/get.js
@@ -1,12 +1,12 @@
-"use strict"
+"use strict";
 
 function get(url) {
     return fetch(url)
         .then(function(response) {
-            return response.json()
+            return response.json();
         })
         .then(function(data) {
-            return data.value;
+            return data;
         })
         .catch(function(error) {
             return error;
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,43 @@
 "use strict";
 
-const getMoreQuotesButton = document.getElementById("getMoreQuotes");
-getMoreQuotesButton.addEventListener("click", function(e) {
-    e.preventDefault();
-    updateChuckSays();
-});
+const chuckForm = document.querySelector("#chuckQuotesForm");
 
-function updateChuckSays() {
+function updateChuckSays(category) {
     const chuckSays = document.getElementById("chuckSays");
 
-    get("https://api.chucknorris.io/jokes/random?category=dev").then(
-        response => {
-            chuckSays.innerHTML = response;
+    get(`https://api.chucknorris.io/jokes/random?category=${category}`).then(
+        function(response) {
+            chuckSays.innerHTML = response.value;
         }
     );
 }
-updateChuckSays();
+
+function getChuckCategories() {
+    const chuckCategorySelect = document.createElement("select");
+
+    const inputWrapper = chuckForm.querySelector("#categorySelect");
+
+    inputWrapper.append(chuckCategorySelect);
+
+    get("https://api.chucknorris.io/jokes/categories").then(function(response) {
+        response.map(function(category) {
+            let categoryOption = document.createElement("option");
+            categoryOption.text = category;
+            categoryOption.value = category;
+            chuckCategorySelect.append(categoryOption);
+        });
+    });
+}
+
+// Immediately invoked Function...
+(function() {
+    chuckForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const categoryValue = this.querySelector("select").value;
+        updateChuckSays(categoryValue);
+    });
+
+    getChuckCategories();
+    updateChuckSays("dev");
+})();
