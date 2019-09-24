24346f26cf8ce73a3a16a649e9ed086994c72c08
Sean R Reid <sean@torchcodelab.com>
2019-09-24 12:00:48 -0400

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,44 @@
 "use strict";
 
-const getMoreQuotesButton = document.getElementById("getMoreQuotes");
-getMoreQuotesButton.addEventListener("click", function(e) {
-    e.preventDefault();
-    updateChuckSays();
+const chuckQuotesForm = document.querySelector("#chuckQuotesForm");
+
+chuckQuotesForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const categoryValue = chuckQuotesForm.querySelector("select").value;
+    updateChuckSays(categoryValue);
 });
 
-function updateChuckSays() {
+function updateChuckSays(category) {
     const chuckSays = document.getElementById("chuckSays");
 
-    get("https://api.chucknorris.io/jokes/random?category=dev").then(
+    get(`https://api.chucknorris.io/jokes/random?category=${category}`).then(
         response => {
-            chuckSays.innerHTML = response;
+            chuckSays.innerHTML = response.value;
         }
     );
 }
-updateChuckSays();
+
+function getCategories() {
+    const selectWrapper = document.querySelector("#selectWrapper");
+    const categoryList = document.createElement("select");
+
+    get(`https://api.chucknorris.io/jokes/categories`).then(function(response) {
+        response.forEach(function(category) {
+            const categoryOption = document.createElement("option");
+            categoryOption.text = category;
+            categoryOption.value = category;
+
+            if (category !== "explicit") {
+                categoryList.append(categoryOption);
+            }
+        });
+    });
+    selectWrapper.append(categoryList);
+}
+
+// Create an IMMEDIATELY INVOKED FUNCTION EXPRESSION, IIFE
+(function() {
+    const defaultCategory = "dev";
+    getCategories();
+    updateChuckSays(defaultCategory);
+})();
