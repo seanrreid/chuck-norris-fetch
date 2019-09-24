fe0dfca90c7f76169a6356a3283ea60024b1a6bc
Kyra Mellema <kemellema@gmail.com>
2019-09-24 09:22:56 -0400

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
 "use strict";
 
 const getMoreQuotesButton = document.getElementById("getMoreQuotes");
-
+const chuckSays = document.getElementById('chuckSays');
+const chuckImage = document.getElementById('chuckImage')
+const icon = document.getElementById('icon')
 // Add an event listener to the button, DON'T FORGET TO PREVENT THE DEFAULT BEHAVIOR!
 // Call a function to return a new quote, and update the DOM
-getMoreQuotesButton.addEventListener("click", function(e) {});
+getMoreQuotesButton.addEventListener("click", function(e) {
+    console.log('button clicked');
+    updateChuckSays('dev');
+});
 
 // Create a function to update the quote text in the DOM
-function updateChuckSays() {}
+function updateChuckSays(category) {
+    const chuckQuote = get(
+        `https://api.chucknorris.io/jokes/random?category=${category}`
+    );
+
+    chuckQuote.then(function(quote) {
+        chuckSays.innerHTML = quote.value;
+        chuckImage.src = quote.icon;
+    });
+}
+
+updateChuckSays('food');
